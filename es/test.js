c33e508d92db039e170a248582861d2b01431ad4
[spark] <[guantao.wu@zhangmen.com]>
2018-09-07 18:34:47 +0800

    save

--- es/test.js
+++ es/test.js
@@ -1,4 +1,4 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import { Icon } from './index';
-ReactDom.render([React.createElement(Icon, { type: 'close' }), React.createElement(Icon, { type: 'calendar-o' })], document.body);
+ReactDom.render([React.createElement(Icon, { type: 'close' }), React.createElement(Icon, { type: 'bell' })], document.body);
